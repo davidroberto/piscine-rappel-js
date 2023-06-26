5ce1fcc6751fd62c4dd9dd06704c382e2c754d4c
David Robert <contact@david-robert.fr>
2023-06-26 16:29:37 +0200

    add attributes

--- index.js
+++ index.js
@@ -3,29 +3,50 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   const divRoot = document.querySelector("#root");
 
-  const articleElement = createNodeElement("article", "article-piscine");
+  const articleElement = createNodeElement("article", {
+    class: "article-piscine",
+  });
   divRoot.appendChild(articleElement);
 
-  const titleElement = createNodeElement("h1", "title-piscine", "Hello La Piscine");
+  const titleElement = createNodeElement(
+    "h1",
+    {
+      class: "title-piscine",
+    },
+    "Hello La Piscine"
+  );
   articleElement.appendChild(titleElement);
 
   const imgElement = createNodeElement(
     "img",
-    "img-piscine",
-    "",
-    "https://www.activateurdeprogres.fr/sites/default/files/activator_form/Piscine-x-EGS.jpg"
+    {
+      class: "img-piscine",
+      src: "https://www.activateurdeprogres.fr/sites/default/files/activator_form/Piscine-x-EGS.jpg",
+    },
+    ""
   );
   articleElement.appendChild(imgElement);
 
-  const textElement = createNodeElement("p", "text-piscine", "lorem ipsum");
+  const textElement = createNodeElement(
+    "p",
+    {
+      class: "text-piscine",
+    },
+    "lorem ipsum"
+  );
   articleElement.appendChild(textElement);
 });
 
-const createNodeElement = (tagType, className, text = "", imgSrc = "") => {
+const createNodeElement = (tagType, attributes, text = "") => {
   const nodeElement = document.createElement(tagType);
-  nodeElement.setAttribute("class", className);
 
-  nodeElement.setAttribute("src", imgSrc);
+  // on fait une boucle sur l'objet attributes
+  // et pour chaque propriété trouvée (class, src etc)
+  // on ajoute un attribut avec en type le nom de la propriété et en valeur sa valeur
+  for (const property in attributes) {
+    nodeElement.setAttribute(property, attributes[property]);
+  }
+
   nodeElement.textContent = text;
 
   return nodeElement;
