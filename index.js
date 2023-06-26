f3234460ac4563fd783550daf82e1d94a8c59c39
David Robert <contact@david-robert.fr>
2023-06-26 14:17:09 +0200

    refactorisation createNodeElement

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const button = document.querySelector("button");
 
-// au click sur le bouton
-
 button.addEventListener("click", () => {
-  // je cible l'élément dans le DOM quk a l'id root
   const divRoot = document.querySelector("#root");
 
-  // je créé une balise article
-  const articleElement = document.createElement("article");
-  articleElement.setAttribute("class", "article-piscine");
+  const articleElement = createNodeElement("article", "article-piscine");
   divRoot.appendChild(articleElement);
 
-  // créer un titre
-  const titleElement = document.createElement("h1");
-  titleElement.textContent = "Hello La Piscine";
-  titleElement.setAttribute("class", "title-piscine");
+  const titleElement = createNodeElement("h1", "title-piscine", "Hello La Piscine");
   articleElement.appendChild(titleElement);
 
-  const imgElement = document.createElement("img");
-  imgElement.setAttribute(
-    "src",
+  const imgElement = createNodeElement(
+    "img",
+    "img-piscine",
+    "",
     "https://www.activateurdeprogres.fr/sites/default/files/activator_form/Piscine-x-EGS.jpg"
   );
-  imgElement.setAttribute("class", "img-piscine");
   articleElement.appendChild(imgElement);
 
-  const textElement = document.createElement("p");
-  textElement.setAttribute("class", "text-piscine");
-  textElement.textContent = "lorem ipsum";
+  const textElement = createNodeElement("p", "text-piscine", "lorem ipsum");
   articleElement.appendChild(textElement);
 });
+
+const createNodeElement = (tagType, className, text = "", imgSrc = "") => {
+  const nodeElement = document.createElement(tagType);
+  nodeElement.setAttribute("class", className);
+
+  nodeElement.setAttribute("src", imgSrc);
+  nodeElement.textContent = text;
+
+  return nodeElement;
+};
